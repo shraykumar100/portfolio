d986b167f60f837bac2f61b3a29bb9657c5834bc
shray kumar <shraykumar100@gmail.com>
2023-05-13 21:28:03 +0530

    initial-commit

--- src/components/home/LiveProjects.jsx
+++ src/components/home/LiveProjects.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 import Container from "react-bootstrap/Container";
 import { Jumbotron } from "./migration";
 import Row from "react-bootstrap/Row";
